cda0151360cbe8ce36704c4d8b8392b4d920e33e
Keisuke Sakaguchi <118953016+ksakaguchi17@users.noreply.github.com>
2026-03-05 15:09:39 +0000

    Add equals test

---
+++ project/record-class/src/jp/exercise/java/CompactDisc.java
@@ -0,0 +1,5 @@
+package jp.exercise.java;
+
+public record CompactDisc(int trackNum) {
+    
+}
--- project/record-class/src/jp/exercise/java/Main.java
+++ project/record-class/src/jp/exercise/java/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
         LongPlaying record = new LongPlaying("My First Album", "ks");
         System.out.println(record);
+
+        LongPlaying record2 = new LongPlaying("My First Album", "ks");
+        System.out.println("record == record2: " + (record == record2) );
+        System.out.println("record.equals(record2): " + (record.equals(record2)) );
+
+        CompactDisc cd1 = new CompactDisc(12);
+        CompactDisc cd2 = new CompactDisc(12);
+        System.out.println("cd1 == cd2: " + (cd1 == cd2) );
+        System.out.println("cd1.equals(cd2): " + (cd1.equals(cd2)) );
     }
 }
